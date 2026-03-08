d40df8c2169d6487120ff211f64a9ad82b38409c
Amashi <amashigg03@gmail.com>
2026-03-08 17:31:29 +0530

    Updated the code to get the client type as an input

--- BillCal.py
+++ BillCal.py
@@ -1,2 +1,4 @@
-units = int(input("Enter electricity units used: "))
+print("Electricity Bill Calculation")
+client_type = input("Enter consumer type (domestic/corporate/religious): ")
+units = float(input("Enter electricity units used: "))
 bill = 0
